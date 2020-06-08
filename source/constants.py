45c952cf1bdd03c2d57e51c624a43bfc99d72abd
Andy Barrett <apbarret@nsidc.org>
2020-06-08 17:41:59 -0600

    add reanalysis grid definitions

--- source/constants.py
+++ source/constants.py
@@ -12,7 +12,9 @@ srcGridFile = {'CFSR2': '/disks/arctic5_raid/abarrett/CFSR2/TOTPREC/2011/01/CFSR
                'MERRA': '/disks/arctic5_raid/abarrett/MERRA/daily/PRECTOT/1979/01/MERRA100.prod.PRECTOT.assim.tavg1_2d_flx_Nx.19790101.nc4',
                'ERAI': '/disks/arctic5_raid/abarrett/ERA_Interim/daily/PRECTOT/1979/01/era_interim.PRECTOT.19790101.nc',
                'MERRA2': '/disks/arctic5_raid/abarrett/MERRA2/daily/PRECTOT/1980/01/MERRA2_100.tavg1_2d_flx_Nx.PRECTOT.19800101.nc4',
-               'ASR': '',}
+               'ASR': '',
+               'ERA5': '/projects/arctic_scientist_data/Reanalysis/ERA5/daily/TOTPREC_global/2000/01/era5.single_level.TOTPREC.20000101.nc4',
+               'ERA5-45N': '/projects/arctic_scientist_data/Reanalysis/ERA5/daily/TOTPREC/1979/01/era5.single_level.daily.TOTPREC.197901.nc4',}
 
 dstGridFile = {'EASE_NH50km': os.path.join(DATADIR, 'EASE_NH50km_ESMF.nc'),}
 
